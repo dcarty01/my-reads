7fbffbb1fb6631596719e456856c550465256e73
Denise Carty <denisecarty@comcast.net>
2018-11-06 05:52:27 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
-export const sortAllBooks = (list) => {
-    // Sort the list of books passed in and return the sorted list
-    const newList = list.sort(function (a, b) {
-      const titleA = a
-        .title
-        .toUpperCase();
-      const titleB = b
-        .title
-        .toUpperCase();
-      if (titleA < titleB) {
-        return -1;
-      }
-      if (titleA > titleB) {
-        return 1;
-      }
-      return 0;
-    })
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
-    return newList;
-  }
-
-export const mergeShelfAndSearch = (shelf, search) => {
-  // For each book in the search results, check if it already
-  // exists in the shelf data
-  const hashTable = {};
-  shelf.forEach(book => hashTable[book.id] = book.shelf);
-
-  search.forEach(book => {
-      book.shelf = hashTable[book.id] || 'none';
-  });
-
-  return search;
-}
+ReactDOM.render(<App />, document.getElementById('root'))
